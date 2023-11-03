48c9655cca71a31be402eff4e5816da8090b5c12
Juanjo Martínez <juanjodevs@gmail.com>
2023-11-03 11:02:20 +0100

    Google signin added

--- app/components/auth-button-client.jsx
+++ app/components/auth-button-client.jsx
@@ -2,17 +2,21 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
-import { IconBrandGithubFilled, IconLogout } from '@tabler/icons-react'
+import { IconBrandGithubFilled, IconBrandGoogle, IconLogout } from '@tabler/icons-react'
 
 export default function AuthButtonClient ({ session }) {
   const supabase = createClientComponentClient()
   const router = useRouter()
 
-  const handleSignIn = async () => {
+  const handleSignIn = async (provider) => {
     await supabase.auth.signInWithOAuth({
-      provider: 'github',
+      provider,
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent'
+        }
       }
     })
   }
@@ -23,12 +27,17 @@ export default function AuthButtonClient ({ session }) {
   }
 
   return (
-    <>
+    <div className='flex flex-col gap-2'>
       {
         session
           ? <button onClick={handleSignOut}><IconLogout size={24} color={'#f87171'}/></button>
-          : <button onClick={handleSignIn} className='flex flex-row justify-center gap-2 border rounded-md border-slate-600 p-2'><IconBrandGithubFilled size={24} />Login with Github</button>
+          : (
+            <>
+              <button onClick={() => handleSignIn('github')} className='flex flex-row justify-center gap-2 border rounded-md border-slate-600 p-2'><IconBrandGithubFilled size={24} />Login with Github</button>
+              <button onClick={() => handleSignIn('google')} className='flex flex-row justify-center gap-2 border rounded-md border-slate-600 p-2'><IconBrandGoogle size={24} />Login with Google</button>
+            </>
+          )
       }
-    </>
+    </div>
   )
 }
